f0e0e065c163024f1d73d9a9b67075ca190f4da1
wangyong-chengdu <wangyong_chengdu@163.com>
2020-11-10 08:14:49 +0800

    pipeline implement.

---
+++ tool/src/test/java/cd/wangyong/tool/test/pipeline/string_append_tasks/AbstractStringTask.java
@@ -0,0 +1,42 @@
+package cd.wangyong.tool.test.pipeline.string_append_tasks;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+
+import cd.wangyong.tool.pipeline.PipeTaskNode;
+import cd.wangyong.tool.pipeline.PipeValueObj;
+
+/**
+ * @author andy
+ * @since 2020/10/23
+ */
+public abstract class AbstractStringTask implements PipeTaskNode {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractStringTask.class);
+
+    @Override
+    public String businessName() {
+        return "string_append_pipeline";
+    }
+
+    @Override
+    public PipeValueObj execute(List<PipeValueObj> inputs) {
+        logger.info("Node-{}:task execute.", id());
+        try {
+            TimeUnit.SECONDS.sleep(2);
+
+            if (CollectionUtils.isEmpty(inputs)) {
+                return PipeValueObj.success("Node-" + id() + " execute success.\n");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            inputs.forEach(pipeValueObj -> sb.append(pipeValueObj.getValue()));
+            return PipeValueObj.success(sb.append("Node-").append(id()).append(" execute success.\n").toString());
+        } catch (Throwable t) {
+            return PipeValueObj.fail(String.format("Node-%d execute fail.", id()), t);
+        }
+    }
+}
---
+++ tool/src/test/java/cd/wangyong/tool/test/pipeline/string_append_tasks/TaskTwo.java
@@ -0,0 +1,13 @@
+package cd.wangyong.tool.test.pipeline.string_append_tasks;
+
+/**
+ * @author andy
+ * @since 2020/10/23
+ */
+public class TaskTwo extends AbstractStringTask {
+
+    @Override
+    public int id() {
+        return 2;
+    }
+}
